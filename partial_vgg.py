6a28a44693ead57ce023f62ac6e74db2fc661bf4
Iris Kutsyy <ak2149@cam.ac.uk>
2021-11-25 19:53:41 +0000

    Added code to generate dataset from images, load pretrained_vgg

--- partial_vgg.py
+++ partial_vgg.py
@@ -1,71 +1,26 @@
-# From https://github.com/msyim/VGG16/blob/master/VGG16.py, modified according to ChromaGAN
-
-import torch.nn as tnn
-import collections
-
-
-def conv_layer(chann_in, chann_out, k_size, p_size):
-    layer = tnn.Sequential(
-        tnn.Conv2d(chann_in, chann_out, kernel_size=k_size, padding=p_size),
-        tnn.BatchNorm2d(chann_out),
-        tnn.ReLU()
-    )
-    return layer
+import torch
+import torch.optim as optim
+import torchvision
+import torch.nn as nn
+import torchvision.transforms as transforms
+from datetime import datetime
+from torch.utils.data import DataLoader
+import torchvision.models as models
 
 
-def vgg_conv_block(in_list, out_list, k_list, p_list, pooling_k, pooling_s):
-    layers = [conv_layer(in_list[i], out_list[i], k_list[i], p_list[i]) for i in range(len(in_list))]
-    layers += [tnn.MaxPool2d(kernel_size=pooling_k, stride=pooling_s)]
-    return tnn.Sequential(*layers)
-
-
-def vgg_fc_layer(size_in, size_out, name=None):
-    if name == None:
-        layer = tnn.Sequential(
-            tnn.Linear(size_in, size_out),
-            tnn.BatchNorm1d(size_out),
-            tnn.ReLU()
-        )
-    else:
-        layer = tnn.Sequential(
-            collections.OrderedDict(
-                [
-                    name + '_linear', tnn.Linear(size_in, size_out),
-                    name + '_batch', tnn.BatchNorm1d(size_out),
-                    name + '_relu', tnn.ReLU()
-                ]
-            )
-        )
-    return layer
-
-
-class VGG16(tnn.Module):
-    def __init__(self, full_model=True):
-        super(VGG16, self).__init__()
-        self.full_model = full_model
-        # Conv blocks (BatchNorm + ReLU activation added in each block)
-        self.layer1 = vgg_conv_block([3, 64], [64, 64], [3, 3], [1, 1], 2, 2)
-        self.layer2 = vgg_conv_block([64, 128], [128, 128], [3, 3], [1, 1], 2, 2)
-        self.layer3 = vgg_conv_block([128, 256, 256], [256, 256, 256], [3, 3, 3], [1, 1, 1], 2, 2)
-        self.layer4 = vgg_conv_block([256, 512, 512], [512, 512, 512], [3, 3, 3], [1, 1, 1], 2, 2)
-        self.layer5 = vgg_conv_block([512, 512, 512], [512, 512, 512], [3, 3, 3], [1, 1, 1], 2, 2)
-
-        if self.full_model:
-            # FC layers
-            self.layer6 = vgg_fc_layer(7 * 7 * 512, 4096, name='top1')
-            self.layer7 = vgg_fc_layer(4096, 4096, name='top2')
-
-            # Final layer
-            self.top3_linear = tnn.Linear(4096, 1000)
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
 
     def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = self.layer5(out)
-        if self.full_model:
-            out = self.layer6(out)
-            out = self.layer8(out)
-            out = self.top3_linear(out)
-        return out
+        return x
+
+def get_partial_vgg():
+    # Expects images normalized via
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                                  std=[0.229, 0.224, 0.225])
+    vgg16 = models.vgg16(pretrained=True)
+    # Strip top layers
+    vgg16.avgpool = Identity()
+    vgg16.classifier = Identity()
+    return vgg16
--- train_gan.py
+++ train_gan.py
@@ -3,10 +3,14 @@ import math
 
 import torch
 from torch import nn
+import torchvision
+import os
+import cv2
+from pathlib import Path
+import random
 
 # Hyperparams (move to text file later)
 import partial_vgg
-import train_vgg
 
 
 class Configuration():
@@ -21,21 +25,44 @@ class Configuration():
 config = Configuration()
 
 
-def get_pretrained_partial_vgg():
-    model = partial_vgg.VGG16(full_model=False)
-    pretrained_dict = torch.load('weights/vgg.pkl')
-    model_dict = model.state_dict()
-    # 1. filter out unnecessary keys
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
-    # 2. overwrite entries in the existing state dict
-    model_dict.update(pretrained_dict)
-    pretrained_dict = [(k, v) for k, v in pretrained_dict.items()]
-    pretrained_dict = collections.OrderedDict(pretrained_dict)
-    # 3. load the new state dict
-    model.load_state_dict(pretrained_dict)
-    return model
+def convert_videos_to_images(eval_portion):
+    Path("dataset/UCF101Images_train").mkdir(parents=True, exist_ok=True)
+    Path("dataset/UCF101Images_eval").mkdir(parents=True, exist_ok=True)
+    total = len(os.listdir("dataset/UCF101"))
+    for i, filename in enumerate(os.listdir("dataset/UCF101")):
+        trimmed_filename = filename[:-4]
+        print(str(100*i/total)+"%,  "+filename)
+        # Skip if already processed this video
+        if not filename.endswith(".avi") or \
+                os.path.isfile("dataset/UCF101Images_train/" + str(trimmed_filename) + "_0.png") \
+                or os.path.isfile("dataset/UCF101Images_eval/" + str(trimmed_filename) + "_0.png"):
+            continue
+
+        frames, audio, metadata = torchvision.io.read_video("dataset/UCF101/" + filename)
+        # Place entire videos either as eval or train, to avoid over-fitting
+        do_eval = random.random() < eval_portion
+        # Reduce training set to 2 fps to save space/time
+        if not do_eval:
+            frames = frames[::12]
+
+        for i, img in enumerate(frames):
+            tensor = cv2.cvtColor(img.cpu().numpy(), cv2.COLOR_BGR2RGB)
+            if do_eval:
+                cv2.imwrite("dataset/UCF101Images_eval/" + str(trimmed_filename) + "_" + str(i) + ".png", tensor)
+            else:
+                cv2.imwrite("dataset/UCF101Images_train/" + str(trimmed_filename) + "_" + str(i) + ".png", tensor)
+
+
+# Use the UCF101 dataset, here https://www.crcv.ucf.edu/data/UCF101.php
+# or here: http://www.thumos.info/download.html
+# This is a collection of human actions, chosen because many black and white videos
+# focus on people (old movies, etc)
+def load_trainset():
+    convert_videos_to_images(0.05)
 
 
 def train_gan():
-    train_set, test_set = train_vgg.load_imagenet(config.batch_size, config.nThreads)
-    vgg = get_pretrained_partial_vgg()
+    vgg16 = partial_vgg.get_partial_vgg()
+
+
+convert_videos_to_images(0.05)
